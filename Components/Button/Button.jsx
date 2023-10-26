9f31195ea7ae405b97168354dde3c454bb34ad8b
Aniket <aniketkumarpaul1@gmail.com>
2023-10-26 14:58:48 +0530

    Added Button Component

--- Components/Button/Button.jsx
+++ Components/Button/Button.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+// INTERNAL IMPORT
+import Style from "./Button.module.css"
+
+const Button = ({ disconnect, connect, address, file }) => {
+  return (
+    <>
+      {
+        address ? (
+          <button onClick={() => disconnect()} className={Style.button}>
+            <span className={Style.button_content}>
+              {" "}
+              {file ? "Upload" : "Disconnect"}
+              {" "}
+            </span>
+          </button>
+        ) : (
+          <button onClick={() => connect()} className={Style.button}>
+            <span className={Style.button_content}>
+              Connect
+            </span>
+          </button>
+        )
+      }
+    </>
+  );
 };
 
 export default Button;
--- pages/layout.js
+++ pages/layout.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 // INTERNAL IMPORTS
-import { Logo } from '../Components'
+import { Button, Logo } from '../Components'
 
 const layout = () => {
   return (
     <div className='home'>
       <Logo />
+      <Button />
     </div>
   )
 }
