435fa6f2c54f49b23458101e065aad433adeb756
Aniket <aniketkumarpaul1@gmail.com>
2023-10-27 12:15:00 +0530

    Added CheckBox Component

--- Components/CheckBox/CheckBox.jsx
+++ Components/CheckBox/CheckBox.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const CheckBox = () => {
-  return <div>CheckBox</div>;
+// INTERNAL IMPORT
+import Style from "./CheckBox.module.css"
+
+const CheckBox = ({ category, setCategory }) => {
+  return (
+    <label
+      className={Style.material_checkbox}
+      onClick={() => setCategory(category)}
+    >
+      <input type="checkbox" />
+      <span className={Style.checkmark}></span>
+      {category}
+    </label>
+  );
 };
 
 export default CheckBox;
--- pages/layout.js
+++ pages/layout.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 // INTERNAL IMPORTS
-import { Button, Card, Footer, Logo } from "../Components";
+import { Button, Card, CheckBox, Footer, Logo } from "../Components";
 
 const layout = () => {
   return (
     <div className="home">
       <Logo />
+
       <p>BUTTON</p>
       <Button />
+
       <p>CARD</p>
       <Card />
+
+      <p>CHECKBOX</p>
+      <CheckBox />
+
       <p>FOOTER</p>
       <Footer />
     </div>
